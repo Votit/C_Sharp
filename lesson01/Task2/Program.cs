5e86cc977435d614461346ffeeb197953110781c
Ivan_Titov <titovid91@gmail.com>
2024-03-06 21:19:32 +0300

    add lesson01:Task2

---
+++ lesson01/Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿int [] numbers = new int [5];
+
+for (int i = 0; i < numbers.Length; i++) {
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < numbers.Length; i++) {
+    Console.Write(numbers[i] + " ");
+} 
