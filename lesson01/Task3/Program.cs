b8d8f177692a593c13ed53d3a6136b48eb8d81b6
Ivan_Titov <titovid91@gmail.com>
2024-03-10 20:59:23 +0300

    add lesson01:Task3

--- lesson01/Task3/Program.cs
+++ lesson01/Task3/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System.Xml;
+
+Console.WriteLine("Enter array length: ");
+int n = int.Parse(Console.ReadLine());
+int[] arr = new int[n];
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = i;
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 == 0) 
+    {
+        Console.Write($"{arr[i]} ");
+        count++;
+    }
+}
+
+Console.WriteLine(count);
